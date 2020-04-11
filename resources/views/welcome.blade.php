15866b7d38c68b0b85650f03a935920592568d04
mzs0229 <mzs0229@hotmail.com>
2020-04-11 11:44:11 +0000

    install font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel BBS
                 </div>
 
                 <div class="links">
