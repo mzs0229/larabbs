1829b8895dbf9df52736d94460a7867bbd2910a6
mzs0229 <mzs0229@hotmail.com>
2020-04-12 06:56:34 +0000

    分类模型和数据

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+		 \App\Models\Project::class => \App\Policies\ProjectPolicy::class,
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
